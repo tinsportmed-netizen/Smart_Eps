2d86c88451a2e63d11d3cadeb1ac2d162e6f1925
tinsportmed-netizen <tinsportmed@gmail.com>
2026-05-28 21:06:32 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Smart EPS Service Worker - Network First + Update Notification
+const CACHE = 'smart-eps-v15';
+const APP_VERSION = '2.1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+  // Notify all open clients of update
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => client.postMessage({type:'UPDATE_AVAILABLE',version:APP_VERSION}));
+  });
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE).then(cache => cache.put(e.request, clone));
+      return resp;
+    }).catch(() => caches.match(e.request))
+  );
+});
